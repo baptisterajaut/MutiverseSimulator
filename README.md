dcc36c1d3dcfa19c46b0eb971321e426f3748224
rajaut <Thisisme11>
2018-12-11 15:00:21 +0100

    final version, supposedly.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MutiverseSimulator
+**Disclaimer : This is not supposed to be a standalone program, but more of POC.  
+It compiles and runs but does nothing important nor interesting.**  
+Structure of a sytsem that may be used to simulate universes and multiverses.  
+Initially made to prove a point.  
+Licensed under MPLv2.
--- src/Examples/MultiverseExample.java
+++ src/Examples/MultiverseExample.java
@@ -1,18 +1,15 @@
 package Examples;
 
-import Structures.Physics.MultiversePhysics.Multiverse;
-import Structures.Physics.MultiversePhysics.MultiversePhysics;
 import Examples.BasicPhysicsExamples.Basic2DPhysics;
 import Examples.BasicPhysicsExamples.Basic3DPhysics;
-
-import java.util.ArrayList;
+import Structures.Physics.MultiversePhysics.Multiverse;
+import Structures.Physics.MultiversePhysics.MultiversePhysics;
 
 public class MultiverseExample extends Multiverse {
-    private MultiversePhysics multiversePhysics;
+
 
     public MultiverseExample(MultiversePhysics physics) {
         super(physics);
-        verses = new ArrayList<>();
 
         verses.add(new BasicMagicUniverseExample(Basic2DPhysics.getPhysics(), 5));
         verses.add(new BasicMagicUniverseExample(Basic3DPhysics.getPhysics(), 7));
